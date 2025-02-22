e3da1923186ed68c0cabe94ee4e26196f352efd2
Infina <49457443+InfinaMii@users.noreply.github.com>
2025-02-22 12:54:59 +0000

    Update TinyFileDialog.cs
    - Add Documents path to fileName if fileName doesn't contain the file's directory (would previously still cause the path splitting issue as the directory was only being added if fileName was null)

--- src/Dialogs/FileDialog/TinyFileDialog.cs
+++ src/Dialogs/FileDialog/TinyFileDialog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Runtime.InteropServices;
 using Toolbox.Core;
 
@@ -40,7 +41,8 @@ namespace MapStudio.UI
         public static string SaveFileDialog(List<FileFilter> filters, string fileName)
         {
             string[] filterList = toFilterArray(filters);
-            fileName ??= Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (Path.GetDirectoryName(fileName) is not { Length: > 0 })
+                fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), fileName);
             return stringFromAnsi(tinyfd_saveFileDialog("Save File", fileName, filterList.Length, filterList, "")); ;
         }
 
